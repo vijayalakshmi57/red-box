82480924a17137d993dbc873928add94048eea6d
vijayalakshmi57 <vijayalaskhmi57@gmail.com>
2024-12-28 11:49:36 +0530

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './Conatiner/Header';
+import Table from './Conatiner/Table';
+
+const Home = () => {
+  return (
+    <div>
+      <Header/>
+      <Table/>
+    </div>
+  )
+}
+
+export default Home;
+
+
---
+++ Orders.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Orders = () => {
+  return (
+    <div>
+      <center>
+        <h4>Orders Component</h4>
+      </center>
+    </div>
+  )
+}
+
+export default Orders;
